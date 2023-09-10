f9d0d501f6adf37a75a6212b7256836e0cbe0e45
rahulkusingh2 <rahulkumarsingh.24msg@gmail.com>
2023-09-10 14:46:54 +0530

    Final Commit

--- src/main/java/com/herokku/internet/Elementstest.java
+++ src/main/java/com/herokku/internet/Elementstest.java
@@ -11,45 +11,49 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Elementstest {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println(" ** Test Started **");
-        WebDriver driver = new ChromeDriver();
-        System.out.println("Browser opened");
-        driver.manage().window().maximize();
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+                System.out.println(" ** Test Started **");
+                WebDriver driver = new ChromeDriver();
+                System.out.println("Browser opened");
+                driver.manage().window().maximize();
 
-        driver.get("https://elementalselenium.com/");
+                driver.get("https://elementalselenium.com/");
 
-        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+                Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        Activities(driver, wait);
-    }
-
-    public static void Activities(WebDriver driver, Wait<WebDriver> wait) {
-        driver.findElement(By.xpath("//a[normalize-space()='External Resources']")).click();
-
-        wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='External Resources']")));
-        if (driver.getCurrentUrl().equals("https://elementalselenium.com/resources")) {
-            System.out.println(driver
-                    .findElement(By.xpath(
-                            "//div[@class='content-text']//h2//a[@href='https://www.selenium.dev/documentation/']"))
-                    .getText());
-
-        } else {
-            System.out.println("Code failed!!!>>>Retry");
+                Activities(driver, wait);
         }
 
-        driver.findElement(By.xpath("//a[@href='https://www.selenium.dev/documentation/']//span[contains(text(),'➔')]"))
-                .click();
+        public static void Activities(WebDriver driver, Wait<WebDriver> wait) {
+                driver.findElement(By.xpath("//a[normalize-space()='External Resources']")).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//a[@href='https://www.selenium.dev/documentation/']//span[contains(text(),'➔')]")));
+                wait.until(
+                                ExpectedConditions.visibilityOfElementLocated(
+                                                By.xpath("//a[normalize-space()='External Resources']")));
+                if (driver.getCurrentUrl().equals("https://elementalselenium.com/resources")) {
+                        System.out.println(driver
+                                        .findElement(By.xpath(
+                                                        "//div[@class='content-text']//h2//a[@href='https://www.selenium.dev/documentation/']"))
+                                        .getText());
 
-        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='td-content'] h1")).getText(),
-                "The Selenium Browser Automation Project");
+                } else {
+                        System.out.println("Code failed!!!>>>Retry");
+                }
 
-        driver.close();
-    }
+                driver.findElement(By.xpath(
+                                "//a[@href='https://www.selenium.dev/documentation/']//span[contains(text(),'➔')]"))
+                                .click();
+
+                wait.until(ExpectedConditions.visibilityOfElementLocated(
+                                By.xpath("//a[@href='https://www.selenium.dev/documentation/']//span[contains(text(),'➔')]")));
+
+                Assert.assertEquals(driver.findElement(By.cssSelector("div[class='td-content'] h1")).getText(),
+                                "The Selenium Browser Automation Project");
+
+                driver.close();
+
+                System.out.println(" ** Test Completed **");
+        }
 
 }

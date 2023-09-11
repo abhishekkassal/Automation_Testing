be7a04eca30d0c6c39f805c04a84ce28a05f835f
kaviyadevimahendran <142013492+kaviyadevimahendran@users.noreply.github.com>
2023-09-11 04:21:43 +0000

    commit on java class

--- src/main/java/brokenimages/test_brokenimages.java
+++ src/main/java/brokenimages/test_brokenimages.java
@@ -1,19 +1,6 @@
 package brokenimages;
  
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.List;
-import org.openqa.selenium.By;
-import java.net.URL;
- 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 public class test_brokenimages
 {
